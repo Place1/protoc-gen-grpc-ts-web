dac7ce396ffbeef4a21547a25574bc680f38dd45
James Batt <getjamesbatt@gmail.com>
2020-01-14 11:49:49 +1100

    wip

--- Makefile
+++ Makefile
@@ -3,11 +3,11 @@ build:
 
 test: build
 	mkdir -p out
-	protoc --ts-web_out=out --plugin=protoc-gen-ts-web --js_out=import_style=commonjs:out ./e2e/example.proto
+	protoc --ts-web_out=out --plugin=protoc-gen-ts-web ./e2e/example.proto
 
 tmp:
 	mkdir -p out_other
-	protoc --js_out=import_style=commonjs:out_other --grpc-web_out=import_style=typescript,mode=grpcwebtext:out_other --plugin=protoc-gen-grpc-web ./e2e/example.proto
+	protoc --js_out=import_style=commonjs:out_other ./e2e/example.proto
 
 clean:
 	rm -r out || true
--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"github.com/place1/protoc-gen-ts-web/templates"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 
-	"github.com/pkg/errors"
+	"github.com/place1/protoc-gen-ts-web/templates"
+
 	"github.com/golang/protobuf/proto"
 	plugin "github.com/golang/protobuf/protoc-gen-go/plugin"
+	"github.com/pkg/errors"
 )
 
 func main() {
@@ -18,17 +20,19 @@ func main() {
 		log.Fatal(errors.Wrap(err, "reading input"))
 	}
 
-	req := plugin.CodeGeneratorRequest{}
-	if err := proto.Unmarshal(data, &req); err != nil {
+	req := &plugin.CodeGeneratorRequest{}
+	if err := proto.Unmarshal(data, req); err != nil {
 		log.Fatal(errors.Wrap(err, "bad codegen request"))
 	}
+
+	depLookupTable := dependencyLookupTable(req)
 
 	res := &plugin.CodeGeneratorResponse{}
 	for _, f := range req.ProtoFile {
 		if err != nil {
 			log.Fatal(errors.Wrap(err, "failed to generate output file"))
 		}
-		res.File = append(res.File, templates.NewFile(f)...)
+		res.File = append(res.File, templates.NewFile(f, depLookupTable)...)
 	}
 
 	out, err := proto.Marshal(res)
@@ -38,3 +42,13 @@ func main() {
 	os.Stdout.Write(out)
 }
 
+func dependencyLookupTable(req *plugin.CodeGeneratorRequest) map[string]string {
+	lookup := map[string]string{}
+	for _, f := range req.ProtoFile {
+		for _, m := range f.GetMessageType() {
+			qualified := fmt.Sprintf(".%s.%s", f.GetPackage(), m.GetName())
+			lookup[qualified] = f.GetName()
+		}
+	}
+	return lookup
+}
